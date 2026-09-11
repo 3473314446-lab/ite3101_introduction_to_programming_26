b7198f9ff7955e3e5e9cd2d028d42bed88685b6f
WUZHENYU-2627 <3473314446@qq.com>
2026-09-11 08:04:29 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -5,4 +5,4 @@ fish_in_clarks_pond = fish_in_clarks_pond - numnber_of_fish_caught
 money_in_wallet = 40
 sandwich_price = 7.50
 sales_tax = .08 * sandwich_price\
-sandwich_price 
+sandwich_price += sales_tax
