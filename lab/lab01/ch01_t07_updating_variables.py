7d5e0f1a75f14c75f69c148bece0fa90258100df
WUZHENYU-2627 <3473314446@qq.com>
2026-09-11 08:03:56 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -2,4 +2,6 @@ fish_in_clarks_pond = 50
 print(Catching fish)
 number_of_fish_caught = 10
 fish_in_clarks_pond = fish_in_clarks_pond - numnber_of_fish_caught
-
+money_in_wallet = 40
+sandwich_price = 7.50
+sales_tax = 
