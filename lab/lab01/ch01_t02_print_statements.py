a5625ed1c453d8b93fbdd086321ccd3e5e28b152
WUZHENYU-2627 <3473314446@qq.com>
2026-09-11 08:18:30 +0000

    [skip actions] lab/lab01/ch01_t02_print_statements.py

--- lab/lab01/ch01_t02_print_statements.py
+++ lab/lab01/ch01_t02_print_statements.py
@@ -1,1 +1,1 @@
-print("Deep into disant woodlands winds a mazy wa， ")
+print("Deep into disant woodlands winds a mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue. ")
