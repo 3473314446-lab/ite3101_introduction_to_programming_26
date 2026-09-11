01697026b6a5e20fe987f748d1658374993e867a
WUZHENYU-2627 <3473314446@qq.com>
2026-09-11 08:55:20 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -2,4 +2,5 @@ cucumners = 100
 num_people = 6
 whole_cucumbers_per_person = 100 / 6
 #the value of quptient is 16 , even though the result of the diviision here is 16.7
-whole_cucumbers_per_person
+whole_cucumbers_per_person = 100/ 6
+#the value of 
