0fa0c39124f90165c960fc621ae484dfff2e0184
WUZHENYU-2627 <3473314446@qq.com>
2026-09-11 08:05:24 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,5 +1,5 @@
 fish_in_clarks_pond = 50
-print("Catching fish)
+print("Catching fish")
 number_of_fish_caught = 10
 fish_in_clarks_pond = fish_in_clarks_pond - numnber_of_fish_caught
 money_in_wallet = 40
