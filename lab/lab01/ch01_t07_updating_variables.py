d2f17fa9ed916ad3e30f623b3c311fca751c63bb
WUZHENYU-2627 <3473314446@qq.com>
2026-09-15 02:09:50 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,6 +1,6 @@
 annual_rainfall = 100
-september = 1.
-october = 3
-november = 3
+september = 20
+october = 15
+november = 10
 december = 2.5
 annual_rainfall = annual_rainfall + september + october + november + december
