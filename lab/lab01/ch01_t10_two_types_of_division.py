85736684e151afd42964e904094859b5df57a990
WUZHENYU-2627 <3473314446@qq.com>
2026-09-16 03:24:34 +0000

    lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -1,8 +1,6 @@
-cumbers_per_person = cucumners // num_people
-print(whole_cucumberucumners = 100
+cucumbers = 100
 num_people = 6
-whole_cucs_per_person)
-#the value of quotient is 16 , even though the result of the diviision here is 16.7
-float_cucumbers_per_person = cucumners/ num_people
+whole_cucumbers_per_person = cucumbers // num_people
+print(whole_cucumbers_per_person)
+float_cucumbers_per_person = cucumbers / num_people
 print(float_cucumbers_per_person)
-#the value of quotient is 16.7
