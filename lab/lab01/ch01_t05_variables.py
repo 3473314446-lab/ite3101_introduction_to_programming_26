62b29a66d8732b00fe08cb47aadbfc6aecf69b3f
WUZHENYU-2627 <3473314446@qq.com>
2026-09-16 03:22:08 +0000

    [skip actions] lab/lab01/ch01_t05_variables.py

--- lab/lab01/ch01_t05_variables.py
+++ lab/lab01/ch01_t05_variables.py
@@ -1,3 +1,3 @@
-greeting_message = "Welcome to Codeacademy!"
-current_excercise = 5
-todays_date = 20260918
+greeting_message = "Welcome to Codecademy!"
+current_exercise = 5
+todays_date = 20260916
