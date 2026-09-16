410083117df1318040f0bb8418a6fe976eeee618
WUZHENYU-2627 <3473314446@qq.com>
2026-09-16 03:28:44 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,2 +1,5 @@
-age_is_12 = True
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+# age_is_12 = True
 name_is_maria = True
