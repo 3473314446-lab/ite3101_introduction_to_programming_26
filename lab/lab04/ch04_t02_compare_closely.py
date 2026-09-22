4c85db3453b1dee22c0368d949d1c643772ec382
WUZHENYU-2627 <3473314446@qq.com>
2026-09-22 02:14:40 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -1,16 +1,16 @@
 # Assign True or False as appropriate on the lines below!
 
 # Set this to True if 17 < 328 or to False if it is not.
-bool_one =   # We did this one for you!
+bool_one = True # We did this one for you!
 
 # Set this to True if 100 == (2 * 50) or to False otherwise.
-bool_two = None
+bool_two = True
 
 # Set this to True if 19 <= 19 or to False if it is not.
-bool_three = None
+bool_three = False
 
 # Set this to True if -22 >= -18 or to False if it is not.
-bool_four = None
+bool_four = N
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
 bool_five = None
