6e05cdf7946245331046709c03c013e284f98962
WUZHENYU-2627 <3473314446@qq.com>
2026-09-22 03:08:07 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -6,7 +6,7 @@ def grade_converter(grade: int) -> str:
         return "B"
     elif grade >= 70:
         return "C"
-    elif grade >= 659:
+    elif grade >= 60:
         return "D"
     else:
         return "F"
