f410ed53fb9a6d578ee6f8b8a506b49060e32560
WUZHENYU-2627 <3473314446@qq.com>
2026-09-22 02:12:41 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -14,7 +14,6 @@ bool_four = None
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
 bool_five = None
-
 (bool_one >> > 17 < 328 True >> > 328 < 17 False)
 (bool_two >> > 100 == ((2 * 50)) True >> > 200 == (2 * 50) False)
 (bool_three >> > 19 <= 19 True >> > 18 <= 19 False)
