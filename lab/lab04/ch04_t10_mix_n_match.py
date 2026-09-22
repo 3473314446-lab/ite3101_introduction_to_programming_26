a350c1b14d15e6be7ed05ceb6e0654642c787460
WUZHENYU-2627 <3473314446@qq.com>
2026-09-22 02:39:09 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -7,7 +7,7 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"
 bool_two = (2 <= 2) and "Alpha" == "Alpha"
 
 # Make me false!
-bool_three = (2 <= 2) and "Alex" == "Bravo"
+bool_three = (3<= 2) and "Alex" == "Bravo"
 
 # Make me true!
 bool_four = 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
