19e472d0a7d8a3c90771a95337202d99112fe3e0
WUZHENYU-2627 <3473314446@qq.com>
2026-09-22 03:06:33 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -1,15 +1,16 @@
 # Complete the if and elif statements!
 def grade_converter(grade: int) -> str:
-    if grade >=90:
+    if grade >= 90:
         return "A"
-    elif grade >=80-89:
+    elif grade >= 80-89:
         return "B"
-    elif grade >=70-79:
+    elif grade >= 70-79:
         return "C"
-    elif grade >=65-69:
+    elif grade >= 65-69:
         return "D"
-    else:grade <=65:
-        return "F"     
+    else:
+        grade:
+        return "F"
 
 
 # This should print an "A"
