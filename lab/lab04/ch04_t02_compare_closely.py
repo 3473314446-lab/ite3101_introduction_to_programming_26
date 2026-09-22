a3d75fa76522e5490f0f311568279569f2e0c857
WUZHENYU-2627 <3473314446@qq.com>
2026-09-22 02:03:44 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -15,4 +15,4 @@ bool_four = None
 # Set this to True if 99 != (98 + 1) or to False otherwise.
 bool_five = None
 
-print (bool_one(>>>17<328 True >>> 328<17 F))
+print (bool_one(>>>17<328 True >>> 328<17 False))
