bd136eb1cc6df81e9266f554ec80ed6595150a13
WUZHENYU-2627 <3473314446@qq.com>
2026-09-22 02:08:02 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -17,4 +17,4 @@ bool_five = None
 
 print (bool_one >>> 17 < 328 True >>> 328 < 17 False);
 print (bool_two >>> 100 == ( (2 * 50)) True >>> 200 ==(2 * 50) False );
-print (bool_three) >>> 19 <= 19 True >>> 
+print (bool_three) >>> 19 <= 19 True >>> 18 <= 19 False
