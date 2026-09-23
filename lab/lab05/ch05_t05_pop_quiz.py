9fabff06b38df38132c7307fc460c65ce69e5733
WUZHENYU-2627 <3473314446@qq.com>
2026-09-23 03:32:23 +0000

    [skip actions] lab/lab05/ch05_t05_pop_quiz.py

--- lab/lab05/ch05_t05_pop_quiz.py
+++ lab/lab05/ch05_t05_pop_quiz.py
@@ -3,7 +3,7 @@ print('Welcome to the Pig Latin Translator!')
 # Start coding here!
 original = input("Enter a word:")
 
-if len(original) = jc124 and original.isalpha():
+if len(original)  and original.isalpha():
     print(original)
 else:
     print("empty")
