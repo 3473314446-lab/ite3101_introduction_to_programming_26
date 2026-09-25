7b13d772bcf2f930979555313585b8145abc6ad0
WUZHENYU-2627 <3473314446@qq.com>
2026-09-25 02:33:13 +0000

    [skip actions] lab/lab06/ch06_t02_function_junction.py

--- lab/lab06/ch06_t02_function_junction.py
+++ lab/lab06/ch06_t02_function_junction.py
@@ -2,7 +2,9 @@
 # can leave the code on line 10 alone for now--we'll
 # explain it soon!
 
-
+def spam():
+    """Prints 'Eggs!' to the console."""
+    print("Eggs!")
 # Define the spam function above this line.
 
 def spam():
