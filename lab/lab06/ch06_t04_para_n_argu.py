c269ac1da789c2e468f3cd61b343d12776929a18
WUZHENYU-2627 <3473314446@qq.com>
2026-09-25 02:56:00 +0000

    [skip actions] lab/lab06/ch06_t04_para_n_argu.py

--- lab/lab06/ch06_t04_para_n_argu.py
+++ lab/lab06/ch06_t04_para_n_argu.py
@@ -1,6 +1,7 @@
 def power():  # Add your parameters here!
     result =  base**exponent 
-    print("%d to the power of %d is %d." % (37, exponent, result))
+    print("%d to the power of %d is %d." % (
+base, exponent, result))
 
 
 power(37, 4)  # Add your arguments here!
